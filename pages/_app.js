95c71dffe35a5295fb5bd8d0c07ae0e40485d9dc
RITIKHARIANI <ritikhariani@gmail.com>
2020-07-11 01:53:31 +0530

    Converted to Typescript

--- pages/_app.js
+++
@@ -1,5 +0,0 @@
-// When _app.js file is added you need to restart the server
-import '../styles/global.css';
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
