e8b2d0dadfb899fb6fba157798a2ecb63b307c3a
RITIKHARIANI <ritikhariani@gmail.com>
2020-07-11 00:51:04 +0530

    First Commit

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+// When _app.js file is added you need to restart the server
+import '../styles/global.css';
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
